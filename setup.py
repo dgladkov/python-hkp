fb395a0e348ba642747135c8c6e8b15def8f13a4
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2010-11-24 19:53:29 +0200

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+Python HKP procol client implementation based on the
+`current draft spec <http://werkzeug.pocoo.org/>`_.
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='python-hkp',
+    version='0.1',
+    url='https://github.com/dgladkov/python-hkp/',
+    license='BSD',
+    author='Dmitry Gladkov',
+    author_email='dmitry.gladkov@gmail.com',
+    description='Python HKP client',
+    long_description=__doc__,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=['hkp'],
+    platforms='any'
+)
